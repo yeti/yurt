41a9ed630641d1b5b0ab04067edfd8a7ef010883
Dean Mercado <dean@yeti.co>
2015-12-21 16:01:49 -0800

    [WIP] Initial Setup for Later Work:
    - Include a current copy of the Ansible system we've been using
    - Replace some Ansible configs
    - Include a `requirements.txt` file
    - Create context managers that work with virtualenvwrapper and bash
    - Added Documentation

---
+++ fabfile/deploy.py
@@ -0,0 +1,64 @@
+import os
+from fabric.operations import local
+from contextlib import contextmanager
+from fabric.context_managers import prefix
+
+__author__ = 'deanmercado'
+
+
+# Context Managers
+@contextmanager
+def bash():
+    with prefix('source ~/.bashrc'):
+        yield
+
+
+@contextmanager
+def ansible():
+    with prefix('workon ansible'):
+        yield
+
+###
+# Hidden/Helper functions
+###
+
+
+def _install_virtualenvwrapper():
+    """
+    Installs virtualenvwrapper globally, adding to $HOME/.bashrc the required lines
+    :return: Void
+    """
+    try:
+        local('sudo pip install virtualenvwrapper')
+    except Exception, e:
+        print 'virtualenvwrapper already installed'
+
+    if not os.path.exists("./.added_to_bashrc"):
+        local('echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc')
+        local('echo "export PROJECT_HOME=$HOME/Devel" >> ~/.bashrc')
+        local('echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc')
+        local("touch .added_to_bashrc")
+
+###
+# Commands that could be called through fab
+###
+
+
+def create_ansible_env():
+    """
+    Creates the ansible environment if it hasn't been created already
+    :return: Void
+    """
+    try:
+        _install_virtualenvwrapper()
+    except Exception, e:
+        print str(e)
+    try:
+        bash('mkvirtualenv ansible')
+        ansible('pip install ansible')
+    except Exception, e:
+        print "Ansible environment exists already!"
+
+
+def load_orchestration():
+    local('cp -rf ./orchestration ../')
