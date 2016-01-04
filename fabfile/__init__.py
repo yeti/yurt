82e25929a298090a0c1873081ce378a433f9d4ed
Dean Mercado <dean@yeti.co>
2016-01-04 08:30:06 -0800

    Use `@task` to identify FAB tasks

---
+++ fabfile/__init__.py
@@ -0,0 +1,1 @@
+from deploy import add_fab_path_to_bashrc, new
--- fabfile/deploy.py
+++ fabfile/deploy.py
@@ -1,12 +1,16 @@
 import os
+from fabric.decorators import task
 from fabric.operations import local
 from contextlib import contextmanager
 from fabric.context_managers import prefix
 
 __author__ = 'deanmercado'
 
-
+###
 # Context Managers
+###
+
+
 @contextmanager
 def bash():
     with prefix('source ~/.bashrc'):
@@ -33,17 +37,29 @@ def _install_virtualenvwrapper():
     except Exception, e:
         print 'virtualenvwrapper already installed'
 
-    if not os.path.exists("./.added_to_bashrc"):
+    if not os.path.exists("$HOME/.added_virtualenvwrapper_to_bashrc"):
         local('echo "export WORKON_HOME=$HOME/.virtualenvs" >> ~/.bashrc')
         local('echo "export PROJECT_HOME=$HOME/Devel" >> ~/.bashrc')
         local('echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc')
-        local("touch .added_to_bashrc")
+        local("touch $HOME/.added_to_bashrc")
+
+
+@task
+def add_fab_path_to_bashrc():
+    """
+    Adds FAB_PATH variable to .bashrc
+    :return: Void
+    """
+    with prefix('cd fabfile'):
+        if not os.path.exists("$HOME/.added_fabpath_to_bashrc"):
+            local('echo "export FAB_PATH=$(pwd -P)" >> ~/.bashrc')
 
 ###
 # Commands that could be called through fab
 ###
 
 
+@task
 def create_ansible_env():
     """
     Creates the ansible environment if it hasn't been created already
@@ -61,4 +77,13 @@ def create_ansible_env():
 
 
 def load_orchestration():
+    """
+    Copies orchestration directory to current directory
+    :return:
+    """
     local('cp -rf ./orchestration ../')
+
+
+@task
+def new():
+    pass
