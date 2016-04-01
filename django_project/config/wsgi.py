733d811c4be1af89674d328c4510f71e45d72838
Dean Mercado <dean@yeti.co>
2016-03-31 18:34:33 -0700

    Fix diabolical `wsgi.py` issue

--- django_project/config/wsgi.py
+++ django_project/config/wsgi.py
@@ -1,8 +1,12 @@
 import os
-
+import sys
+​
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "%(vagrant.settings_path)s")
-
+​
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+​
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+​
+sys.path.append(os.path.abspath(os.path.join(PROJECT_ROOT, "../../")))
+​
 application = get_wsgi_application()
-
