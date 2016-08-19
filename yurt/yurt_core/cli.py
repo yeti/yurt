8a232fe8f6093cf57b424f363bc12ac86c7493c1
Dean Mercado <dean@yeti.co>
2016-08-19 16:49:32 -0700

    Release-0.1.0
    - BUG_FIX: Fixes issue with command-less release
    - Increment version number

--- yurt/yurt_core/cli.py
+++ yurt/yurt_core/cli.py
@@ -5,5 +5,8 @@ import click
 def main():
     pass
 
+from yurt.yurt_core.setup import new_project, create_pem_file, copy_pem_file, existing
+from yurt.yurt_core.add import remote_server, vault
+
 if __name__ == '__main__':
     main()
