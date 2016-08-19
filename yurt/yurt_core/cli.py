d777d40078bed1e8f7b37711ca19271b596767b1
Dean Mercado <dean@yeti.co>
2016-08-19 13:36:57 -0700

    ENHANCE: Add email configs to `yurt remote_server`
    - Standardize the `?` in question
    - Fix import and subcommand breakout issues

--- yurt/yurt_core/cli.py
+++ yurt/yurt_core/cli.py
@@ -1,13 +1,10 @@
 import click
+from yurt.yurt_core.add import add
+from yurt.yurt_core.deploy import deploy_cli
+from yurt.yurt_core.setup import setup
 
+main = click.CommandCollection(sources=[add, deploy_cli, setup])
 
-@click.group()
-def main():
-    pass
-
-from yurt.yurt_core.setup import new_project, create_pem_file, copy_pem_file, existing
-from yurt.yurt_core.add import remote_server, vault
-from yurt.yurt_core.deploy import deploy
 
 if __name__ == '__main__':
     main()
