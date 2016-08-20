a49271cf569f45f13bef55497cb0ee46117a26f5
Dean Mercado <dean@yeti.co>
2016-08-19 17:30:20 -0700

    Release-0.1.2
    - CHORE: Increment version number
    - BUG FIX: Add `deploy` command to `yurt` entrypoint

--- yurt/yurt_core/cli.py
+++ yurt/yurt_core/cli.py
@@ -7,6 +7,7 @@ def main():
 
 from yurt.yurt_core.setup import new_project, create_pem_file, copy_pem_file, existing
 from yurt.yurt_core.add import remote_server, vault
+from yurt.yurt_core.deploy import deploy
 
 if __name__ == '__main__':
     main()
