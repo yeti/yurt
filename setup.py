5ea5830d213e26f28464be73b7c094b7abda3a46
Dean Mercado <dean@yeti.co>
2016-11-04 14:28:37 -0700

    CHORE: Increment version number again

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     packages=find_packages(),
     package_data={'yurt': get_package_data_files()},
     include_package_data=True,
-    version="0.1.9",
+    version="0.1.10",
     description="A tool for deploying Django Web Apps to remote servers",
     long_description=long_description,
     url="https://github.com/yeti/yurt/",
