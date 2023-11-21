711255c59023f63adc21b3b326477d03a5f3a033
Will Harris <will.t.harris@protonmail.com>
2023-11-21 12:00:19 -0800

    wip

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,11 +1,7 @@
 // import * as Sentry from "@sentry/node";
-// import { NODE_ENV } from "./constants";
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-// import { auth } from 'express-oauth2-jwt-bearer';
-// import { unless } from "./utils/unless";
-// import { DATABASE_URL, NODE_ENV } from './config';
 
 // Sentry.init({ dsn: SENTRY_DSN });
 
@@ -23,16 +19,6 @@ app.use(
   }),
 );
 
-// Authorization middleware. Access token must be in the headers in the following format:
-// Authorization: "Bearer {accessToken}"
-// const checkJwt = auth({
-//   audience,
-//   issuerBaseURL,
-// });
-// app.use(unless(["/status", "/forest"], checkJwt));
-
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 // app.use(Sentry.Handlers.errorHandler());
 
 app.get('/status', (_req, res) => {
