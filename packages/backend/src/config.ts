8a802ad8b7d91b1b00ce1f9b95a850a733471610
Will Harris <will.t.harris@protonmail.com>
2023-11-21 15:00:53 -0800

    wip

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -2,15 +2,15 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
+  DATABASE_URL: z.string().url(),
+  LOG_LEVEL: z.string(),
   NODE_ENV: z.string(),
   PORT: z.string(),
-  LOG_LEVEL: z.string(),
-  DATABASE_URL: z.string().url(),
 });
 
 const env = envSchema.parse(process.env);
 
+export const DATABASE_URL = env.DATABASE_URL;
+export const LOG_LEVEL = env.LOG_LEVEL;
 export const NODE_ENV = env.NODE_ENV;
 export const PORT = env.PORT;
-export const LOG_LEVEL = env.LOG_LEVEL;
-export const DATABASE_URL = env.DATABASE_URL;
