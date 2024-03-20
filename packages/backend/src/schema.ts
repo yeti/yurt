62d48182f130ac99c9c3e4f16705eb896d7eeeae
Will Harris <will.t.harris@protonmail.com>
2024-03-20 14:26:23 -0700

    refactor: remove use of __dirname global

--- packages/backend/src/schema.ts
+++ packages/backend/src/schema.ts
@@ -2,6 +2,10 @@ import { makeSchema } from 'nexus';
 import { NODE_ENV } from '~/config';
 import * as types from '~/schemaTypes';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const schema = makeSchema({
   types,
