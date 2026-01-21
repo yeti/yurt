4adf25093983fb6bbed8e7ca9dfa148f08c01188
Will Harris <will.t.harris@protonmail.com>
2026-01-21 13:36:16 -0800

    wip

--- packages/backend/src/tests/setup.ts
+++
@@ -1,7 +0,0 @@
-import { beforeEach } from 'vitest';
-import { mockReset } from 'vitest-mock-extended';
-import { prismaMock } from '~/prismaSingleton';
-
-beforeEach(() => {
-  mockReset(prismaMock);
-});
